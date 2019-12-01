41637be7620bbcb61c800326c23f2546cd8c0410
Jimmy Calderon <JimmyCalderon@Jimmys-MacBook-Pro.local>
2019-12-01 02:34:05 -0500

    added databse and added sass files

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,11 @@
 (()=> {
     
-
     
+    const waypoint = new Waypoint({
+        element: document.getElementById('driver1'),
+        handler: function(direction) {
+          console.log('Scrolled to waypoint!')
+        
+        }
+      })
 })();
