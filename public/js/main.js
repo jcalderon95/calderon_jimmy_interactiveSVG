0a185c09b0aec8976421c4c626634ec6783fa435
Jimmy Calderon <JimmyCalderon@Jimmys-MacBook-Pro.local>
2019-12-01 22:03:59 -0500

    fixed bugs

--- public/js/main.js
+++ public/js/main.js
@@ -3,21 +3,26 @@
     const tl   = gsap.timeline();
 
     
-    const waypoint = new Waypoint({
+    const waypoint1 = new Waypoint({
         element: document.getElementById('driver1'),
         handler: function() {
+            console.log('Scrolled to waypoint!')
             let url = `/drivers/${this.element.id.slice(6)}`,
-                car = this.element.children[0].children[2],
+                imageTarget = this.element.children[0].children[2],
                 driverCon = this.element.children[1];
-
-                tl.from(car, .8,{y:90,  opacity: 0})
-                tl.from(driverCon, .5,{y:20,  opacity: 0})
-                car.classList.add('car-on');
-
+            
                 fetch(url)
                     .then(res => res.json())
                     .then(data => {
                     console.log(data);
+
+                    tl.from(imageTarget, 1,{y:100,  opacity: 0})
+                    tl.from(driverCon, .5,{y:20,  opacity: 0})
+                    
+                    imageTarget.innerHTML += `
+                    <img src="images/${data.Image}" alt="${data.Team} Car Image" class="car">
+                    `
+                    
                     driverCon.innerHTML += `
                     <h2 class="driverName">${data.Name}</h2>
                     <h3 class="info">NUMBER: <span class="data">${data.Number}</span></h3>
@@ -31,7 +36,7 @@
             
             this.destroy()
         },
-        offset: '30%'
+        offset: "60%"
       })
 
       const waypoint2 = new Waypoint({
@@ -39,17 +44,21 @@
         handler: function() {
             console.log('Scrolled to waypoint!')
             let url = `/drivers/${this.element.id.slice(6)}`,
-                car = this.element.children[0].children[2],
+                imageTarget = this.element.children[0].children[2],
                 driverCon = this.element.children[1];
-
-                tl.from(car, .8,{y:90,  opacity: 0})
-                tl.from(driverCon, .5,{y:20,  opacity: 0})
-                car.classList.add('car-on');
-
+            
                 fetch(url)
                     .then(res => res.json())
                     .then(data => {
                     console.log(data);
+
+                    tl.from(imageTarget, .8,{y:100,  opacity: 0})
+                    tl.from(driverCon, .5,{y:20,  opacity: 0})
+                    
+                    imageTarget.innerHTML += `
+                    <img src="images/${data.Image}" alt="${data.Team} Car Image" class="car">
+                    `
+                    
                     driverCon.innerHTML += `
                     <h2 class="driverName">${data.Name}</h2>
                     <h3 class="info">NUMBER: <span class="data">${data.Number}</span></h3>
@@ -63,25 +72,30 @@
             
             this.destroy()
         },
-        offset: 200
+        offset: "40%"
       })
+
 
       const waypoint3 = new Waypoint({
         element: document.getElementById('driver3'),
         handler: function() {
             console.log('Scrolled to waypoint!')
             let url = `/drivers/${this.element.id.slice(6)}`,
-                car = this.element.children[0].children[2],
+                imageTarget = this.element.children[0].children[2],
                 driverCon = this.element.children[1];
-
-                tl.from(car, .8,{y:90,  opacity: 0})
-                tl.from(driverCon, .5,{y:20,  opacity: 0})
-                car.classList.add('car-on');
-
+            
                 fetch(url)
                     .then(res => res.json())
                     .then(data => {
                     console.log(data);
+
+                    tl.from(imageTarget, .8,{y:100,  opacity: 0})
+                    tl.from(driverCon, .5,{y:20,  opacity: 0})
+                    
+                    imageTarget.innerHTML += `
+                    <img src="images/${data.Image}" alt="${data.Team} Car Image" class="car">
+                    `
+                    
                     driverCon.innerHTML += `
                     <h2 class="driverName">${data.Name}</h2>
                     <h3 class="info">NUMBER: <span class="data">${data.Number}</span></h3>
@@ -95,7 +109,7 @@
             
             this.destroy()
         },
-        offset: 200
+        offset: "40%"
       })
 
       const waypoint4 = new Waypoint({
@@ -103,17 +117,21 @@
         handler: function() {
             console.log('Scrolled to waypoint!')
             let url = `/drivers/${this.element.id.slice(6)}`,
-                car = this.element.children[0].children[2],
+                imageTarget = this.element.children[0].children[2],
                 driverCon = this.element.children[1];
-
-                tl.from(car, .8,{y:30,  opacity: 0})
-                tl.from(driverCon, .5,{y:20,  opacity: 0})
-                car.classList.add('car-on');
-
+            
                 fetch(url)
                     .then(res => res.json())
                     .then(data => {
                     console.log(data);
+
+                    tl.from(imageTarget, .8,{y:100,  opacity: 0})
+                    tl.from(driverCon, .5,{y:20,  opacity: 0})
+                    
+                    imageTarget.innerHTML += `
+                    <img src="images/${data.Image}" alt="${data.Team} Car Image" class="car">
+                    `
+                    
                     driverCon.innerHTML += `
                     <h2 class="driverName">${data.Name}</h2>
                     <h3 class="info">NUMBER: <span class="data">${data.Number}</span></h3>
@@ -127,8 +145,103 @@
             
             this.destroy()
         },
-        offset: 200
+        offset: "40%"
       })
+    //   const waypoint2 = new Waypoint({
+    //     element: document.getElementById('driver2'),
+    //     handler: function() {
+    //         console.log('Scrolled to waypoint!')
+    //         let url = `/drivers/${this.element.id.slice(6)}`,
+    //             car = this.element.children[0].children[2],
+    //             driverCon = this.element.children[1];
+
+    //             tl.from(car, .8,{y:90,  opacity: 0})
+    //             tl.from(driverCon, .5,{y:20,  opacity: 0})
+    //             car.classList.add('car-on');
+
+    //             fetch(url)
+    //                 .then(res => res.json())
+    //                 .then(data => {
+    //                 console.log(data);
+    //                 driverCon.innerHTML += `
+    //                 <h2 class="driverName">${data.Name}</h2>
+    //                 <h3 class="info">NUMBER: <span class="data">${data.Number}</span></h3>
+    //                 <h3 class="info">NATIONALITY: <span class="data">${data.Nationality}</span></h3>
+    //                 <h3 class="info">TEAM: <span class="data">${data.Team}</span></h3>
+    //                 <h3 class="info">POINTS: <span class="data">${data.Points}</span></h3>
+    //                 `
+                    
+    //                 })
+    //                 .catch((err) => console.log(err));
+            
+    //         this.destroy()
+    //     },
+    //     offset: '40%'
+    //   })
+
+    //   const waypoint3 = new Waypoint({
+    //     element: document.getElementById('driver3'),
+    //     handler: function() {
+    //         console.log('Scrolled to waypoint!')
+    //         let url = `/drivers/${this.element.id.slice(6)}`,
+    //             car = this.element.children[0].children[2],
+    //             driverCon = this.element.children[1];
+
+    //             tl.from(car, .8,{y:90,  opacity: 0})
+    //             tl.from(driverCon, .5,{y:20,  opacity: 0})
+    //             car.classList.add('car-on');
+
+    //             fetch(url)
+    //                 .then(res => res.json())
+    //                 .then(data => {
+    //                 console.log(data);
+    //                 driverCon.innerHTML += `
+    //                 <h2 class="driverName">${data.Name}</h2>
+    //                 <h3 class="info">NUMBER: <span class="data">${data.Number}</span></h3>
+    //                 <h3 class="info">NATIONALITY: <span class="data">${data.Nationality}</span></h3>
+    //                 <h3 class="info">TEAM: <span class="data">${data.Team}</span></h3>
+    //                 <h3 class="info">POINTS: <span class="data">${data.Points}</span></h3>
+    //                 `
+                    
+    //                 })
+    //                 .catch((err) => console.log(err));
+            
+    //         this.destroy()
+    //     },
+    //     offset: 200
+    //   })
+
+    //   const waypoint4 = new Waypoint({
+    //     element: document.getElementById('driver4'),
+    //     handler: function() {
+    //         console.log('Scrolled to waypoint!')
+    //         let url = `/drivers/${this.element.id.slice(6)}`,
+    //             car = this.element.children[0].children[2],
+    //             driverCon = this.element.children[1];
+
+    //             tl.from(car, .8,{y:30,  opacity: 0})
+    //             tl.from(driverCon, .5,{y:20,  opacity: 0})
+    //             car.classList.add('car-on');
+
+    //             fetch(url)
+    //                 .then(res => res.json())
+    //                 .then(data => {
+    //                 console.log(data);
+    //                 driverCon.innerHTML += `
+    //                 <h2 class="driverName">${data.Name}</h2>
+    //                 <h3 class="info">NUMBER: <span class="data">${data.Number}</span></h3>
+    //                 <h3 class="info">NATIONALITY: <span class="data">${data.Nationality}</span></h3>
+    //                 <h3 class="info">TEAM: <span class="data">${data.Team}</span></h3>
+    //                 <h3 class="info">POINTS: <span class="data">${data.Points}</span></h3>
+    //                 `
+                    
+    //                 })
+    //                 .catch((err) => console.log(err));
+            
+    //         this.destroy()
+    //     },
+    //     offset: 200
+    //   })
 
 
       
